d66c014c1a9f981fd087f47a30ac7ca46fd7e578
gravvi <Cybiak@gmail.com>
2021-02-06 23:31:28 +0100

    dotNetFirstCommit

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "opis działania kreatora wdrożeń";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "adres do firmy Test-sytem";
+
+            return View();
+        }
+
+        public ActionResult CreatorForms()
+        {
+            ViewBag.Message = "Proszę o uzupełnienie formularza";
+
+            return View();
+        }
+        public ActionResult go1()
+        {
+            ViewBag.Message = "css ";
+
+            return View();
+        }
+    }
+}
